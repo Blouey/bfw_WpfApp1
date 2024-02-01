71c712c4dd3dc2dbd9e881fc0aee413e4f3ad60b
Philip Klare <philipklare@gmail.com>
2024-02-01 18:20:43 +0100

    inital

---
+++ FtoC.xaml.cs
@@ -0,0 +1,66 @@
+﻿using WPFCustomMessageBox;
+using System.Windows;
+
+namespace WpfApp1;
+
+public partial class FtoC : Window
+{
+    public FtoC()
+    {
+        InitializeComponent();
+    }
+    
+    private void BtnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+    
+    private void BtnConvert_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (TxtCelsius.Text != string.Empty && TxtFahrenheit.Text == string.Empty)
+            {
+                var celsius = double.TryParse(TxtCelsius.Text, out var celsiusValue)
+                    ? celsiusValue
+                    : throw new Exception("Invalid Celsius value");
+                var fahrenheit = celsiusValue * 9 / 5 + 32;
+                TxtFahrenheit.Text = fahrenheit.ToString("F2");
+            }
+            else if (TxtCelsius.Text == string.Empty && TxtFahrenheit.Text != string.Empty)
+            {
+                var fahrenheit = double.TryParse(TxtFahrenheit.Text, out var fahrenheitValue)
+                    ? fahrenheitValue
+                    : throw new Exception("Invalid Fahrenheit value");
+                var celsius = (fahrenheitValue - 32) * 5 / 9;
+                TxtCelsius.Text = celsius.ToString("F2");
+            }
+            else if(TxtCelsius.Text != string.Empty && TxtFahrenheit.Text != string.Empty)
+            {
+                MessageBoxResult res = CustomMessageBox.ShowYesNo("Which one do you want to convert?", "°F or °C", "°F -> °C", "°C -> °F"); 
+                if(res == MessageBoxResult.Yes)
+                {
+                    var fahrenheit = double.TryParse(TxtFahrenheit.Text, out var fahrenheitValue)
+                        ? fahrenheitValue
+                        : throw new Exception("Invalid Fahrenheit value");
+                    var celsius = (fahrenheitValue - 32) * 5 / 9;
+                    TxtCelsius.Text = celsius.ToString("F2");
+                }
+                else if(res == MessageBoxResult.No)
+                {
+                    var celsius = double.TryParse(TxtCelsius.Text, out var celsiusValue)
+                        ? celsiusValue
+                        : throw new Exception("Invalid Celsius value");
+                    var fahrenheit = celsiusValue * 9 / 5 + 32;
+                    TxtFahrenheit.Text = fahrenheit.ToString("F2");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+    
+    
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonMessage_OnClick(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Hello World");
+        BtnMessage.Content = "Clicked";
+        BtnMessage.Effect = new BlurEffect();
+        
+        
+    }
+    
+    private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+    
+    private void ButtonMessage_OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        if (BtnMessage.Effect == null)
+        {
+            BtnMessage.Content = "Mouse Enter";
+            BtnMessage.RenderSize = new Size(
+                305, 105);
+        }
+    }
+
+    private void BtnMessage_OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        if (BtnMessage.Effect == null)
+        {
+            BtnMessage.Content = "Mouse left";
+            BtnMessage.RenderSize = new Size(
+                300, 100);
+            Thread.Sleep(1000);
+            BtnMessage.Content = "Message";
+        }
+    }
+}
